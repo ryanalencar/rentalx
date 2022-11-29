83f92143025888f139e1d6f0ba0aea36ce2fb96c
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-11-29 09:12:46 -0300

    feat: typeorm database config

---
+++ src/database/config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "123456",
+  database: "rentx",
+  entities: [],
+  subscribers: [],
+  migrations: [],
+});
