8ed36f7bfc403cffc0df61cd9dd50b143348a1c4
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-12-06 10:54:57 -0300

    refactor: create category use case and controller async functions

--- src/modules/cars/useCases/category/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/category/createCategory/CreateCategoryController.ts
@@ -5,11 +5,10 @@ import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) { }
 
-  handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response): Promise<Response> {
     try {
       const { name, description } = request.body;
-
-      this.createCategoryUseCase.execute({ description, name });
+      await this.createCategoryUseCase.execute({ description, name });
 
       return response.status(201).send();
     } catch (error) {
