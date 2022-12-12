b21b508cd2b6b5ce238503fdf2a2fe29765fc3cc
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-12-12 16:08:11 -0300

    feat: find user by id

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import { User } from '../entitites';
 interface IUsersRepository {
   create(user: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
