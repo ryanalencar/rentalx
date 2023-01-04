f25392109aab62773c2ddb7d8f232a33f81de6b7
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-04 17:25:51 -0300

    ✨ feat(config): implemented multer upload config

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import { uploadConfig } from '../config/upload';
 import { CreateCategoryController } from '../modules/cars/useCases/category/createCategory';
 import { ImportCategoryController } from '../modules/cars/useCases/category/importCategory';
 import { ListCategoriesController } from '../modules/cars/useCases/category/listCategories';
 
 const categoriesRoutes = Router();
 
-const upload = multer({
-  dest: './temp',
-});
+const uploadCategory = multer(uploadConfig.upload('./temp/category'));
 
 const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
@@ -21,7 +20,7 @@ categoriesRoutes.get('/', listCategoriesController.handle);
 
 categoriesRoutes.post(
   '/import',
-  upload.single('file'),
+  uploadCategory.single('file'),
   importCategoryController.handle,
 );
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import { uploadConfig } from '../config/upload';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.patch(
+  '/avatar',
+  uploadAvatar.single('avatar'),
+  updateUserController.handle,
+);
 
 export { usersRoutes };
