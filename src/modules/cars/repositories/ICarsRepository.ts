255d2cb97891e6dd9f120a1e47191a8a7ceb46b4
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-07 19:02:00 -0300

    ✨ feat(cars): find car by license plate

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateCarDTO } from '../dtos';
+import { Car } from '../infra/typeorm/entities';
 
 export interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<void>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
 }
--- test/repositories/CarsRepositoryInMemory.ts
+++ test/repositories/CarsRepositoryInMemory.ts
@@ -5,6 +5,13 @@ import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 export class CarsRepositoryInMemory implements ICarsRepository {
   cars: Car[] = [];
 
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = this.cars.find(
+      (_category) => _category.license_plate === license_plate,
+    );
+    return car;
+  }
+
   async create(data: ICreateCarDTO): Promise<void> {
     const car = new Car();
     Object.assign(car, data);
