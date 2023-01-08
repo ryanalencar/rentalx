833b1f3b35a50def4533f9f1217fa29e5526f0d5
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-08 00:34:24 -0300

    ✨ feat(typeorm): add Car to entities

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,7 +1,11 @@
 import { DataSource } from 'typeorm';
 
 import { User } from '@modules/accounts/infra/typeorm/entities';
-import { Category, Specification } from '@modules/cars/infra/typeorm/entities';
+import {
+  Car,
+  Category,
+  Specification,
+} from '@modules/cars/infra/typeorm/entities';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -10,7 +14,7 @@ export const AppDataSource = new DataSource({
   username: 'postgres',
   password: '123456',
   database: 'rentx',
-  entities: [Category, Specification, User],
+  entities: [Category, Specification, User, Car],
   subscribers: [],
   migrations: ['src/shared/infra/typeorm/migrations/*.ts'],
 });
