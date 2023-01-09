e103af447b6901900707e550f6353a8ddaef17cd
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-09 12:46:57 -0300

    ✨ feat(cars): list available cars with params

--- src/modules/cars/useCases/car/list-available-cars/ListAvailableCarsUsecase.ts
+++ src/modules/cars/useCases/car/list-available-cars/ListAvailableCarsUsecase.ts
@@ -1,5 +1,8 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Car } from '@modules/cars/infra/typeorm/entities';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { Singletons } from '@shared/container';
 
 interface IRequest {
   brand?: string;
@@ -7,8 +10,12 @@ interface IRequest {
   name?: string;
 }
 
+@injectable()
 export class ListAvailableCarsUsecase {
-  constructor(private carsRepository: ICarsRepository) { }
+  constructor(
+    @inject(Singletons.CarsRepository)
+    private carsRepository: ICarsRepository,
+  ) { }
 
   async execute({ brand, category_id, name }: IRequest): Promise<Car[]> {
     const cars = await this.carsRepository.findAvailable({
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/car/create-car/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/car/list-available-cars/ListAvailableCarsController';
 
 import { isAdmin } from '../middlewares/isAdmin';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
@@ -8,9 +9,9 @@ import { isAuthenticated } from '../middlewares/isAuthenticated';
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
-carsRoutes.use(isAuthenticated, isAdmin);
-
-carsRoutes.post('/', createCarController.handle);
+carsRoutes.get('/available', listAvailableCarsController.handle);
+carsRoutes.post('/', isAuthenticated, isAdmin, createCarController.handle);
 
 export { carsRoutes };
