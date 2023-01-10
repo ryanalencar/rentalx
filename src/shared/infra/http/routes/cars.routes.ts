9769f414c292128d4f9bb31a9e07539079e45870
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-10 12:31:16 -0300

    ✨ feat(car): create car specification route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { CreateCarSpecificationController } from '@modules/cars/useCases/car/create-car-specification/CreateCarSpecificationController';
 import { CreateCarController } from '@modules/cars/useCases/car/create-car/CreateCarController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/car/list-available-cars/ListAvailableCarsController';
 
@@ -10,8 +11,15 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.get('/available', listAvailableCarsController.handle);
 carsRoutes.post('/', isAuthenticated, isAdmin, createCarController.handle);
+carsRoutes.post(
+  '/specifications/:id',
+  isAuthenticated,
+  isAdmin,
+  createCarSpecificationController.handle,
+);
 
 export { carsRoutes };
