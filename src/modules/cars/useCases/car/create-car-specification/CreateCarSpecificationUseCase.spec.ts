8b268509b58192b9efaf0eed19314b2ceba09782
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-10 12:30:38 -0300

    ✨ feat(specification): create car specification use case and controller

--- src/modules/cars/useCases/car/create-car-specification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/car/create-car-specification/CreateCarSpecificationUseCase.spec.ts
@@ -1,35 +1,45 @@
 import { AppError } from '@shared/errors/AppError';
 import { makeCar } from '@test/factories/car-factory';
+import { makeSpecification } from '@test/factories/specification-factory';
 import { CarsRepositoryInMemory } from '@test/repositories/CarsRepositoryInMemory';
+import { SpecificationsRepositoryInMemory } from '@test/repositories/SpecificationsRepositoryInMemory';
 
 import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
-let carsRepository: CarsRepositoryInMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 describe('Create Car Specification', () => {
   beforeEach(() => {
-    const carsRepository = new CarsRepositoryInMemory();
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
-      carsRepository,
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory,
     );
   });
 
-  it('should be able to create a new specification for a car', async () => {
-    const car = await carsRepository.create(makeCar());
-    const specifications = ['1234'];
+  it('should be able to add a new specification for a car', async () => {
+    const car = await carsRepositoryInMemory.create(makeCar());
+    const specification = await specificationsRepositoryInMemory.create(
+      makeSpecification(),
+    );
 
-    await createCarSpecificationUseCase.execute({
+    const specificationsCars = await createCarSpecificationUseCase.execute({
       car_id: car.id,
-      specifications,
+      specifications_id: [specification.id],
     });
+
+    expect(specificationsCars).toHaveProperty('specifications');
+    expect(specificationsCars.specifications).toHaveLength(1);
   });
 
   it('should not be able to create a new specification for a non existing car', async () => {
     expect(async () => {
       await createCarSpecificationUseCase.execute({
         car_id: 'non-existing-car',
-        specifications: ['specification-id'],
+        specifications_id: ['specification-id'],
       });
     }).rejects.toBeInstanceOf(AppError);
   });
--- src/modules/cars/useCases/car/create-car-specification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/car/create-car-specification/CreateCarSpecificationUseCase.ts
@@ -1,22 +1,42 @@
 import { inject, injectable } from 'tsyringe';
 
+import { Car } from '@modules/cars/infra/typeorm/entities';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { Singletons } from '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 import { statusCode } from '@utils/statusCode';
 
 interface IRequest {
   car_id: string;
-  specifications: string[];
+  specifications_id: string[];
 }
 
+@injectable()
 export class CreateCarSpecificationUseCase {
-  constructor(private carsRepository: ICarsRepository) { }
+  constructor(
+    @inject(Singletons.CarsRepository)
+    private carsRepository: ICarsRepository,
 
-  async execute({ car_id, specifications }: IRequest): Promise<void> {
+    @inject(Singletons.SpecificationsRepository)
+    private specificationsRepository: ISpecificationsRepository,
+  ) { }
+
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carExists = await this.carsRepository.findById(car_id);
 
     if (!carExists) {
       throw new AppError('Car does not exists!', statusCode.badRequest);
     }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id,
+    );
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
+
+    return carExists;
   }
 }
