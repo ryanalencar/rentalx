a227d00f8d3a0bc9afa1e0905a3b64a58d72ab2c
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-15 15:24:47 -0300

    🧪 test(cars-category): create category controller test

--- src/modules/cars/useCases/category/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/category/createCategory/CreateCategoryController.spec.ts
@@ -1,14 +1,72 @@
+import { hash } from 'bcryptjs';
 import request from 'supertest';
+import { DataSource } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 
 import { app } from '@shared/infra/http/app';
+import { createConnection } from '@shared/infra/typeorm';
+
+let connection: DataSource;
 
 describe('Create category controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+        values('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'XXXXXX' )
+    `,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.destroy();
+  });
+
   it('it should be able to create a new category', async () => {
-    const response = await request(app).post('categories').send({
-      name: 'Category supertest',
-      description: 'Category supertest',
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com',
+      password: 'admin',
     });
 
+    const { refresh_token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({
+        Authorization: `Bearer ${refresh_token}`,
+      });
+
     expect(response.status).toBe(201);
+  });
+
+  it('should not be able to create a category with the same name', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com',
+      password: 'admin',
+    });
+
+    const { refresh_token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({
+        Authorization: `Bearer ${refresh_token}`,
+      });
+
+    expect(response.status).toBe(400);
   });
 });
