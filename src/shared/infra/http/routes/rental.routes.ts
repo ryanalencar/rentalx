4667e7c9afe47abf40bef0f607abc624eab6033e
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-16 23:54:18 -0300

    ✨ feat(rentals): devolution rental controller and route

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/use-cases/create-rental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/use-cases/devolution-rental/DevolutionRentalController';
 
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
-rentalRoutes.post('/', isAuthenticated, createRentalController.handle);
+rentalRoutes.use(isAuthenticated);
+
+rentalRoutes.post('/', createRentalController.handle);
+rentalRoutes.post('/devolution/:id', devolutionRentalController.handle);
 
 export { rentalRoutes };
