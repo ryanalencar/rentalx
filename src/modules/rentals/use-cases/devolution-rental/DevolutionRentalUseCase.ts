e2715380de6ca7cc1d97c689dde085982c76f6f9
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-16 23:42:27 -0300

    ✨ feat(rentals): devolution rental use case

--- src/modules/rentals/use-cases/devolution-rental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/use-cases/devolution-rental/DevolutionRentalUseCase.ts
@@ -1,3 +1,68 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { Singletons } from '@shared/container';
+import { IDateProvider } from '@shared/container/providers/date-provider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+import { deleteFile } from '@utils/file';
+import { statusCode } from '@utils/statusCode';
+
+interface IRequest {
+  id: string;
+  user_id: string;
+}
+
+@injectable()
 export class DevolutionRentalUseCase {
-  async execute() { }
+  constructor(
+    @inject(Singletons.RentalsRepository)
+    private rentalsRepository: IRentalsRepository,
+    @inject(Singletons.CarsRepository)
+    private carsRepository: ICarsRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
+  ) { }
+
+  async execute({ id, user_id }: IRequest) {
+    const rental = await this.rentalsRepository.findById(id);
+    const car = await this.carsRepository.findById(id);
+
+    const MINIMUM_DAILY = 1;
+
+    if (!rental) {
+      throw new AppError('Rental does not exist', statusCode.notFound);
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    let rentalDaily = this.dateProvider.compareInDays(
+      rental.start_date,
+      dateNow,
+    );
+
+    if (rentalDaily <= 0) {
+      rentalDaily = MINIMUM_DAILY;
+    }
+
+    const rentalDelay = this.dateProvider.compareInDays(
+      dateNow,
+      rental.expected_return_date,
+    );
+
+    let totalRentalPrice = 0;
+    if (rentalDelay > 0) {
+      const calculateFine = rentalDelay * car.fine_amount;
+      totalRentalPrice = calculateFine;
+    }
+    totalRentalPrice += rentalDaily * car.daily_rate;
+
+    rental.end_date = this.dateProvider.dateNow();
+    rental.total = totalRentalPrice;
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
+
+    return rental;
+  }
 }
