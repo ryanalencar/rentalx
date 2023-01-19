0bc2ca3fdf7e57e9a2804c1e3772fde903a3179e
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-19 09:27:40 -0300

    ✨ feat(singletons): added users tokens sigleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,9 @@ import { container } from 'tsyringe';
 import '@shared/container/providers';
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
@@ -22,6 +24,7 @@ export enum Singletons {
   CarsRepository = 'CarsRepository',
   CarsImagesRepository = 'CarsImagesRepository',
   RentalsRepository = 'RentalsRepository',
+  UsersTokensRepository = 'UsersTokensRepository',
 }
 
 container.registerSingleton<ICategoriesRepository>(
@@ -52,4 +55,9 @@ container.registerSingleton<ICarsImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   Singletons.RentalsRepository,
   RentalsRepository,
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  Singletons.UsersTokensRepository,
+  UsersTokensRepository,
 );
