6e6854232257d83ec96e53824b379d764fe2fcf6
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-19 09:28:07 -0300

    ✨ feat(auth): added refresh token

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -5,7 +5,9 @@ import { inject, injectable } from 'tsyringe';
 import { authConfig } from '@config/auth';
 import { ICreateUserDTO } from '@modules/accounts/dtos';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { Singletons } from '@shared/container';
+import { IDateProvider } from '@shared/container/providers/date-provider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
 import { statusCode } from '@utils/statusCode';
 
@@ -17,6 +19,7 @@ interface IAuthenticateUserRequest {
 export interface IAuthenticateUserResponse {
   user: Omit<ICreateUserDTO, 'password' | 'driver_license'>;
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
@@ -24,6 +27,10 @@ export class AuthenticateUserUseCase {
   constructor(
     @inject(Singletons.UsersRepository)
     private usersRepository: IUsersRepository,
+    @inject(Singletons.UsersTokensRepository)
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
   ) { }
 
   async execute({
@@ -55,11 +62,27 @@ export class AuthenticateUserUseCase {
 
     const token = sign({}, secret_token, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: expires_in_token,
+    });
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      expires_refresh_token_days,
+    );
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      expires_date: refresh_token_expires_date,
+      refresh_token,
     });
 
     return {
       token,
+      refresh_token,
       user: {
         email: user.email,
         name: user.name,
