e215fe3eeba4103ff616ad7be3bc3fcfcd82ca10
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-19 15:12:54 -0300

    ✨ feat(accounts): new methods to users tokens repository

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -1,7 +1,10 @@
 import { Repository } from 'typeorm';
 
 import { ICreateUserTokenDTO } from '@modules/accounts/dtos';
-import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import {
+  IFindByUserIdAndRefreshTokenParams,
+  IUsersTokensRepository,
+} from '@modules/accounts/repositories/IUsersTokensRepository';
 import { AppDataSource } from '@shared/infra/typeorm';
 
 import { UserTokens } from '../entities';
@@ -13,9 +16,21 @@ export class UsersTokensRepository implements IUsersTokensRepository {
     this.repository = AppDataSource.getRepository(UserTokens);
   }
 
+  async findByUserIdAndRefreshToken({
+    refresh_token,
+    user_id,
+  }: IFindByUserIdAndRefreshTokenParams): Promise<UserTokens> {
+    const usersToken = this.repository.findOneBy({ user_id, refresh_token });
+    return usersToken;
+  }
+
   async create(data: ICreateUserTokenDTO): Promise<UserTokens> {
     const userToken = this.repository.create(data);
     await this.repository.save(userToken);
     return userToken;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
   }
 }
--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -1,6 +1,15 @@
 import { ICreateUserTokenDTO } from '../dtos/ICreateUserTokenDTO';
 import { UserTokens } from '../infra/typeorm/entities';
 
+export interface IFindByUserIdAndRefreshTokenParams {
+  user_id: string;
+  refresh_token: string;
+}
+
 export interface IUsersTokensRepository {
   create(data: ICreateUserTokenDTO): Promise<UserTokens>;
+  findByUserIdAndRefreshToken({
+    refresh_token,
+  }: IFindByUserIdAndRefreshTokenParams): Promise<UserTokens>;
+  deleteById(id: string): Promise<void>;
 }
