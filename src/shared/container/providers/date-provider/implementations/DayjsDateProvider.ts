8c8e13ed0e5cec2f0b5f7eca463fba387a74211e
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-19 09:23:21 -0300

    ✨ feat(date-provider): addDays method

--- src/shared/container/providers/date-provider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/date-provider/implementations/DayjsDateProvider.ts
@@ -6,12 +6,17 @@ import { IDateProvider } from '../IDateProvider';
 dayjs.extend(utc);
 
 export class DayjsDateProvider implements IDateProvider {
+  addDays(days: number): Date {
+    return dayjs().add(days, 'days').toDate();
+  }
+
   compareInHours(start_date: Date, end_date: Date): number {
     const start_date_utc = this.convertToUTC(start_date);
     const end_date_utc = this.convertToUTC(end_date);
 
     return dayjs(end_date_utc).diff(start_date_utc, 'hours');
   }
+
   compareInDays(start_date: Date, end_date: Date): number {
     const start_date_utc = this.convertToUTC(start_date);
     const end_date_utc = this.convertToUTC(end_date);
