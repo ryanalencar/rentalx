586dc573d6ec32b3a7c927825836850c913651fa
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-21 21:48:09 -0300

    ✨ feat(providers): add compare before to date provider

--- src/shared/container/providers/date-provider/IDateProvider.ts
+++ src/shared/container/providers/date-provider/IDateProvider.ts
@@ -1,6 +1,7 @@
 export interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
   compareInDays(start_date: Date, end_date: Date): number;
+  compareIfBefore(start_date: Date, end_date: Date): boolean;
   convertToUTC(date: Date): string;
   dateNow(): Date;
   addDays(days: number): Date;
--- src/shared/container/providers/date-provider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/date-provider/implementations/DayjsDateProvider.ts
@@ -28,6 +28,11 @@ export class DayjsDateProvider implements IDateProvider {
     return dayjs(end_date_utc).diff(start_date_utc, 'days');
   }
 
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    const isBefore = dayjs(start_date).isBefore(end_date);
+    return isBefore;
+  }
+
   convertToUTC(date: Date): string {
     return dayjs(date).utc().local().format();
   }
