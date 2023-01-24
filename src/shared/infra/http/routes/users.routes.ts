99911ba8a812e8dfe190cb81ab8cf5aa64d2ad75
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-24 09:51:05 -0300

    ✨ feat(accounts): profile user controller and route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import { uploadConfig } from '@config/upload';
 import { CreateUserController } from '@modules/accounts/use-cases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/use-cases/profile-user/ProfileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/use-cases/updateUserAvatar/UpdateUserAvatarController';
 import { isAuthenticated } from '@shared/infra/http/middlewares/isAuthenticated';
 
@@ -12,11 +13,15 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.use(isAuthenticated);
+
+usersRoutes.get('/', profileUserController.handle);
 usersRoutes.patch(
   '/avatar',
-  isAuthenticated,
   uploadAvatar.single('avatar'),
   updateUserController.handle,
 );
