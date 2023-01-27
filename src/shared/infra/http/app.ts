3137c9c17b459bd0074ec295ece165ac5adf7d82
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-27 09:47:58 -0300

    ✨ feat(app): sentry setup

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,8 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import { uploadConfig } from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { router } from '@shared/infra/http/routes';
 
 import swaggerFile from '../../../swagger.json';
@@ -15,12 +17,26 @@ import { handleErrors } from './middlewares/handleErrors';
 import { rateLimiter } from './middlewares/rateLimiter';
 
 import '@shared/container';
+import { statusCode } from '@utils/statusCode';
 
 createConnection();
 
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -30,6 +46,8 @@ app.use('/cars', express.static(`${uploadConfig.tmpFolder}/car-images`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(handleErrors);
 
